310f6aefc33c8304747ea98487795b17330e6517
Olivier <dhondt.olivier@gmail.com>
2014-06-15 13:44:26 +0200

    Coordinate lists

--- PMCompletion.cpp
+++ PMCompletion.cpp
@@ -1,9 +1,9 @@
 /*
  #
- #  File        : PatchMatch.cpp
+ #  File        : PMCompletion.cpp
  #                ( C++ source file )
  #
- #  Description : Example of use for the CImg plugin 'PatchMatch_plugin'.
+ #  Description : Example use for the completion functionality in 'PatchMatch_plugin.h'.
  #                ( http://cimg.sourceforge.net )
  #
  #  Copyright   : Olivier D'Hondt
@@ -50,17 +50,17 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  cimg_usage("PatchMatch: approximate nearest neighbour patch matching between two images img0 and img1.");
+  cimg_usage("PatchMatch based image completion.");
   cimg_help("Input / Output parameters:\n--------------------------");
-  const char *file0 = cimg_option("-i0", (char*)0, "Image 0");
-  const char *file1 = cimg_option("-i1", (char*)0, "Image 1");
+  const char *fileimg = cimg_option("-i", (char*)0, "Image");
+  const char *filemask = cimg_option("-m", (char*)0, "Mask");
   const char *fileout = cimg_option("-o", (char*)0, "Output image of offsets (channel 0: x offset, channel 1: y offset)");
   const int P = cimg_option("-s", 7, "Patch size");
   const int N = cimg_option("-n", 5, "Number of iterations");
   const bool DISP = cimg_option("-d", true, "Real-time display (slows down the process)");
   const bool VERB = cimg_option("-v", true, "Verbose");
 
-  if(!file0 || !file1){
+  if(!fileimg || !filemask){
     cerr<<"Must specify images with -i0 and -i1 options (-h for help).\n";
     exit(0);
   }
@@ -68,36 +68,13 @@ int main(int argc, char **argv)
   srand (time(NULL));
 
 
-  CImg<int> img0(file0);
-  CImg<int> img1(file1);
-  CImg<int> imgrec(img0, "xyzc", 0);
+  CImg<int> img(fileimg);
+  CImg<int> mask(filemask);
+  CImg<int> imgrec(img, "xyzc", 0);
 
-  CImg<int> off;
-
-  if(VERB){
-    cout<<"\n--------\n";
-    cout<<"PatchMatch: approximate nearest neighbor search\n";
-    cout<<"From left to right: \n";
-    cout<<"(a) Image 0 \n";
-    cout<<"(b) Reconstructed version of image 0 from image 1 patches\n";
-    cout<<"(c) HSV displacement map (hue encodes the angle, saturation the magnitude)\n";
-    cout<<"(d) Image 1\n";
-    cout<<"--------\n\n";
-  }
-
-  // Optional display for real-time view
-  CImgDisplay *disp =  0;
-  if(DISP) disp = new CImgDisplay;
-  off.patchMatch(img0, img1, P, N, disp);
-  delete disp;
-  cout<<"Done!\n\n";
-
-  (img0,
-  imgrec.reconstruct(img1, off),
-  off.get_vizFlow(100),
-  img1).display("PatchMatch: final result.", false);
-
-
+//  CImg<int> off;
+  
+  
   if(fileout)
-    off.save_cimg(fileout);
+    imgrec.save_cimg(fileout);
 }
--- PatchMatch_plugin.h
+++ PatchMatch_plugin.h
@@ -220,3 +220,159 @@ template<typename Tt>
 CImg<T> get_reconstruct(const CImg<T> &qimg, const CImg<Tt> &off) const {
   return CImg<T>(*this, false).reconstruct(qimg, off);
 }
+
+// ------------------ Image completion functions ---------------------
+
+// Making lists of target and source pixel coordinates from mask
+template<typename Tt>
+void fillXYLists(const CImg<T> &mask, CImg<Tt> &listSource, CImg<Tt> &listTarget) const {
+  
+  int sizeTarget = 0, sizeSource = 0;
+  cimg_forXY(mask, x, y) {
+    if(mask(x,y) == 0)
+      ++sizeSource;
+    else
+      ++sizeTarget;
+  }
+  
+  listTarget.assign(sizeTarget, 2, 1, 1, 0.0);
+  listSource.assign(sizeSource, 2, 1, 1, 0.0);
+
+  int idxTarget = 0, idxSource = 0;
+  cimg_forXY(mask, x, y) {
+    if(mask(x,y) == 0) {
+      listSource(idxSource,0) = x;
+      listSource(idxSource,1) = y;
+      ++idxSource;
+    } else {
+      listTarget(idxTarget,0) = x;
+      listTarget(idxTarget,1) = y;
+      ++idxTarget;
+    }
+  }
+}
+
+// Version of the patch match algorithm for image completion
+// the target is a hole defined by a binary 
+// mask and the source is the rest of the image
+template<typename Tt>
+CImg<T>& patchMatchCmpl(const CImg<Tt> &img0, const CImg<Tt> &img1,
+                    int patchSize, const int nIter = 2,
+                    CImgDisplay *const disp=0) {
+  if (img0.spectrum() != img1.spectrum())
+    throw CImgInstanceException("Images must have the same number of channels.");
+
+  if (!patchSize % 2){
+    ++patchSize;
+    cimg::warn("Input patch size is even, adding 1.");
+  }
+  const int
+    w0 = img0.width(),
+    h0 = img0.height(),
+    w1 = img1.width(),
+    h1 = img1.height(),
+    nChannels = img0.spectrum();
+
+  CImg<Tt> imgrec, visu0, visu1; // used only for display purpose
+  if (disp) {
+    imgrec.assign(img0.width(),img0.height(),1,img0.spectrum(),0);
+    visu0 = img0.get_resize(-100,-100,1,3);
+    visu1 = img1.get_resize(-100,-100,1,3);
+  }
+
+  const int P = patchSize, H = P/2;
+
+  // Zero padding borders
+  CImg<Tt> img0big(w0+2*H, h0+2*H, 1, nChannels, 0);
+  CImg<Tt> img1big(w1+2*H, h1+2*H, 1, nChannels, 0);
+
+  // Try to penalize border patches
+  img0big.rand(0,255);
+  img1big.rand(0,255);
+
+  img0big.draw_image(H, H, 0, 0, img0);
+  img1big.draw_image(H, H, 0, 0, img1);
+
+  CImg<T> off(w0, h0, 1, 2, 0);
+  CImg<Tt> minDist(w0, h0, 1, 1, 0);
+
+  // Initialize with random offsets
+  cimg_forXY(off, x0, y0){
+    int x1 = ((w1-1) * cimg::rand());
+    int y1 = ((h1-1) * cimg::rand());
+    off(x0, y0, 0) = x1 - x0;
+    off(x0, y0, 1) = y1 - y0;
+    minDist(x0, y0) = distPatch(img0big, img1big, x0, y0, x1, y1, P);
+  }
+
+  int xStart, yStart, xFinish, yFinish, inc;
+  for (int n = 0; n < nIter; ++n) {
+    std::fprintf(stderr,"Iteration %d\n",n+1);
+
+    // at odd iterations, reverse scan order
+    if (!(n%2)) { xStart = yStart = inc = 1; xFinish = w0; yFinish = h0; }
+    else { xStart = w0-2; yStart = h0-2; xFinish = yFinish = inc = -1; }
+
+    for (int y = yStart; y != yFinish; y=y+inc)
+      for (int x = xStart; x != xFinish; x=x+inc) {
+        // Propagation
+        Tt d2 = 0.0;
+        int x1 = x+off(x-inc,y,0), y1 = y+off(x-inc,y,1);
+        if(x1 >= 0 && x1 < w1 && y1 >= 0 && y1 < h1){ // propagate only if inside img1 bounds
+          d2 = distPatch(img0big, img1big, x, y, x1, y1, P);
+          if (d2<minDist(x, y)) {
+            minDist(x, y) = d2;
+            off(x, y, 0) = off(x-inc, y, 0);
+            off(x, y, 1) = off(x-inc, y, 1);
+          }
+        }
+        x1 = x+off(x, y-inc, 0);
+        y1 = y+off(x, y-inc, 1);
+        if (x1 >= 0 && x1 < w1 && y1 >= 0 && y1 < h1) { // propagate only if inside img1 bounds
+          d2 = distPatch(img0big, img1big, x, y, x1, y1, P);
+          if (d2<minDist(x, y)) {
+            minDist(x, y) = d2;
+            off(x, y, 0) = off(x, y-inc, 0);
+            off(x, y, 1) = off(x, y-inc, 1);
+          }
+        }
+
+        // Randomized search
+        int wSizX = w1 - 1, wSizY = h1 - 1;
+        const T offXCurr = off(x, y, 0), offYCurr = off(x, y, 1);
+        do{
+          const int
+            wMinX = cimg::max(0, x+offXCurr-wSizX/2),
+            wMaxX = cimg::min(w1-1, x+offXCurr+wSizX/2);
+          x1 = (wMaxX-wMinX) * cimg::rand() + wMinX;
+
+          const int
+            wMinY = cimg::max(0, y+offYCurr-wSizY/2),
+            wMaxY = cimg::min(h1-1, y+offYCurr+wSizY/2);
+          y1 = (wMaxY-wMinY) * cimg::rand() + wMinY;
+          d2 = distPatch(img0big, img1big, x, y, x1, y1, P);
+
+          if (d2 < minDist(x, y)) {
+            minDist(x, y) = d2;
+            off(x, y, 0) = x1-x;
+            off(x, y, 1) = y1-y;
+          }
+          wSizX /= 2;
+          wSizY /= 2;
+        } while (wSizX >= 1 && wSizY >= 1);
+        // If a pointer to a CImgDisplay is passed as the last argument
+        // the output of the algorithm is displayed as an animation
+        // !! It slows down the algorithm a lot !!
+        if (disp) {
+          if (x%(w0-1)==0)
+            disp->display((visu0,
+                           imgrec.reconstruct(img1, off).get_normalize(0,255).resize(-100,-100,1,3),
+                           off.get_vizFlow(100),
+                           visu1)).set_title("Iteration %d",n+1);
+          if (disp->is_resized()) disp->resize();
+        }
+      }
+  }
+  return off.move_to(*this);
+}
+
